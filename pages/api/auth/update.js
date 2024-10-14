322921bb3fffd21f098346fb66b5bcab6d39af51
rayiqi <ramadifan769@gmail.com>
2024-10-15 06:55:22 +0700

    second

--- pages/api/auth/update.js
+++ pages/api/auth/update.js
@@ -1,46 +1,46 @@
-import bcryptjs from 'bcryptjs';
-import User from '../../../models/User';
-import db from '../../../utils/db';
-import { getToken } from 'next-auth/jwt';
+// import bcryptjs from 'bcryptjs';
+// import User from '../../../models/User';
+// import db from '../../../utils/db';
+// import { getToken } from 'next-auth/jwt';
 
-async function handler(req, res) {
-  if (req.method !== 'PUT') {
-    return res.status(400).send({ message: `${req.method} not supported` });
-  }
+// async function handler(req, res) {
+//   if (req.method !== 'PUT') {
+//     return res.status(400).send({ message: `${req.method} not supported` });
+//   }
 
-  const user = await getToken({ req });
-  if (!user) {
-    return res.status(401).send({ message: 'signin required' });
-  }
+//   const user = await getToken({ req });
+//   if (!user) {
+//     return res.status(401).send({ message: 'signin required' });
+//   }
 
-  const { name, email, password } = req.body;
+//   const { name, email, password } = req.body;
 
-  if (
-    !name ||
-    !email ||
-    !email.includes('@') ||
-    (password && password.trim().length < 5)
-  ) {
-    res.status(422).json({
-      message: 'Validation error',
-    });
-    return;
-  }
+//   if (
+//     !name ||
+//     !email ||
+//     !email.includes('@') ||
+//     (password && password.trim().length < 5)
+//   ) {
+//     res.status(422).json({
+//       message: 'Validation error',
+//     });
+//     return;
+//   }
 
-  await db.connect();
-  const toUpdateUser = await User.findById(user._id);
-  toUpdateUser.name = name;
-  toUpdateUser.email = email;
+//   await db.connect();
+//   const toUpdateUser = await User.findById(user._id);
+//   toUpdateUser.name = name;
+//   toUpdateUser.email = email;
 
-  if (password) {
-    toUpdateUser.password = bcryptjs.hashSync(password);
-  }
+//   if (password) {
+//     toUpdateUser.password = bcryptjs.hashSync(password);
+//   }
 
-  await toUpdateUser.save();
-  await db.disconnect();
-  res.send({
-    message: 'User updated',
-  });
-}
+//   await toUpdateUser.save();
+//   await db.disconnect();
+//   res.send({
+//     message: 'User updated',
+//   });
+// }
 
-export default handler;
+// export default handler;
